28ca72b0cc4e98c93723e53c379757ab6b2810b7
jeancarlogali0128 <jeancarlogali0128@hotmail.com>
2017-12-23 02:32:33 -0500

    primer commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Panel10 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(58, 138)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "ENCIENDE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(58, 167)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "APAGAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(220, 138)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "ENCIENDE"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(220, 167)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "APAGAR"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(377, 138)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "ENCIENDE"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(379, 167)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "APAGAR"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(18, 37)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(62, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.SeaShell
+        Me.Button7.Location = New System.Drawing.Point(12, 12)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "CONECTAR"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(377, 12)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 10
+        Me.Button8.Text = "POR VOZ"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(529, 141)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 23)
+        Me.Button9.TabIndex = 11
+        Me.Button9.Text = "ENCIENDE"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(529, 168)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 12
+        Me.Button10.Text = "APAGAR"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(684, 142)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 13
+        Me.Button11.Text = "ENCIENDE"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(687, 169)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(75, 23)
+        Me.Button12.TabIndex = 14
+        Me.Button12.Text = "APAGAR"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Panel10
+        '
+        Me.Panel10.BackgroundImage = Global.ENCENDER_LED_TRABAJO.My.Resources.Resources.cochera_apagado
+        Me.Panel10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel10.Location = New System.Drawing.Point(660, 196)
+        Me.Panel10.Name = "Panel10"
+        Me.Panel10.Size = New System.Drawing.Size(148, 136)
+        Me.Panel10.TabIndex = 16
+        '
+        'Panel4
+        '
+        Me.Panel4.BackgroundImage = Global.ENCENDER_LED_TRABAJO.My.Resources.Resources.cuarto_apagado
+        Me.Panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel4.Location = New System.Drawing.Point(500, 196)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(150, 136)
+        Me.Panel4.TabIndex = 15
+        '
+        'Panel3
+        '
+        Me.Panel3.BackgroundImage = Global.ENCENDER_LED_TRABAJO.My.Resources.Resources.comedor_apagado
+        Me.Panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel3.Location = New System.Drawing.Point(338, 196)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(157, 136)
+        Me.Panel3.TabIndex = 7
+        '
+        'Panel2
+        '
+        Me.Panel2.BackgroundImage = Global.ENCENDER_LED_TRABAJO.My.Resources.Resources.cocina_apagado
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel2.Location = New System.Drawing.Point(183, 196)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(149, 136)
+        Me.Panel2.TabIndex = 4
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = Global.ENCENDER_LED_TRABAJO.My.Resources.Resources.sala_apagado
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel1.Location = New System.Drawing.Point(7, 195)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(168, 137)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel5
+        '
+        Me.Panel5.BackgroundImage = Global.ENCENDER_LED_TRABAJO.My.Resources.Resources.microfono1
+        Me.Panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel5.Location = New System.Drawing.Point(387, 41)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(58, 63)
+        Me.Panel5.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Bisque
+        Me.ClientSize = New System.Drawing.Size(820, 342)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel10)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents Panel10 As System.Windows.Forms.Panel
+    Friend WithEvents Panel5 As System.Windows.Forms.Panel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,212 @@
+﻿
+Imports System.Speech
+Imports System.Speech.Recognition
+
+Public Class Form1
+
+    Dim REC As New SpeechRecognitionEngine
+    Dim SYNT As New Speech.Synthesis.SpeechSynthesizer
+    Dim PALABRA As String
+
+    Public Sub RECONOCE(ByVal sender As Object, ByVal e As SpeechRecognizedEventArgs)
+        Dim RESULTADO As RecognitionResult
+        RESULTADO = e.Result
+        Dim PALABRA As String
+        PALABRA = RESULTADO.Text
+        If (PALABRA = "prende sala") Then
+            Panel1.BackgroundImage = My.Resources.sala_prendi
+
+            SerialPort1.Write("1")
+
+        ElseIf (PALABRA = "apaga sala") Then
+            Panel1.BackgroundImage = My.Resources.sala_apagado
+
+
+
+            SerialPort1.Write("0")
+
+
+        ElseIf (PALABRA = "prende cocina") Then
+
+            Panel2.BackgroundImage = My.Resources.cocina_prendida
+
+
+            SerialPort1.Write("3")
+
+
+        ElseIf (PALABRA = "apaga cocina") Then
+            Panel2.BackgroundImage = My.Resources.cocina_apagado
+
+
+
+            SerialPort1.Write("2")
+
+        ElseIf (PALABRA = "prende comedor") Then
+            Panel3.BackgroundImage = My.Resources.comedor_prendido
+
+
+            SerialPort1.Write("5")
+
+        ElseIf (PALABRA = "apaga comedor") Then
+            Panel3.BackgroundImage = My.Resources.comedor_apagado
+
+
+
+            SerialPort1.Write("4")
+
+        ElseIf (PALABRA = "prende cuarto") Then
+            Panel4.BackgroundImage = My.Resources.cuarto_prendido
+
+
+
+
+            SerialPort1.Write("7")
+
+        ElseIf (PALABRA = "apaga cuarto") Then
+            Panel4.BackgroundImage = My.Resources.cuarto_apagado
+
+
+
+            SerialPort1.Write("6")
+
+        ElseIf (PALABRA = "prende cochera") Then
+            Panel10.BackgroundImage = My.Resources.cochera_prendido
+
+
+
+
+            SerialPort1.Write("9")
+
+        ElseIf (PALABRA = "apaga cochera") Then
+            Panel10.BackgroundImage = My.Resources.cochera_apagado
+
+
+
+            SerialPort1.Write("8")
+
+
+
+
+
+        End If
+    End Sub
+
+
+
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        Try
+            SerialPort1.PortName = TextBox1.Text
+            SerialPort1.Open()
+
+            SerialPort1.Write("0")
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SerialPort1.Write("1")
+        Panel1.BackgroundImage = My.Resources.sala_prendi
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        SerialPort1.Write("0")
+        Panel1.BackgroundImage = My.Resources.sala_apagado
+
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If Button8.Text = "POR VOZ" Then
+            Try
+
+                Dim VOCABULARIO As New GrammarBuilder
+                VOCABULARIO.Append(New Choices("prende sala", "apaga sala", "prende cocina", "apaga cocina", "prende comedor", "apaga comedor", "prende cuarto", "apaga cuarto", "prende cochera", "apaga cochera"))
+                REC.LoadGrammar(New Grammar(VOCABULARIO))
+
+                REC.SetInputToDefaultAudioDevice()
+                REC.RecognizeAsync(RecognizeMode.Multiple)
+                AddHandler REC.SpeechRecognized, AddressOf RECONOCE
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+            Button8.Text = "HABLE AHORA"
+
+            Panel5.BackgroundImage = My.Resources.microfono2
+
+        ElseIf Button8.Text = "HABLE AHORA" Then
+
+            Panel5.BackgroundImage = My.Resources.microfono1
+
+            Button8.Text = "POR VOZ"
+
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SerialPort1.Write("3")
+        Panel2.BackgroundImage = My.Resources.cocina_prendida
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        SerialPort1.Write("2")
+
+        Panel2.BackgroundImage = My.Resources.cocina_apagado
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        SerialPort1.Write("5")
+        Panel3.BackgroundImage = My.Resources.comedor_prendido
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        SerialPort1.Write("4")
+        Panel3.BackgroundImage = My.Resources.comedor_apagado
+
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        SerialPort1.Write("7")
+        Panel4.BackgroundImage = My.Resources.cuarto_prendido
+
+
+    End Sub
+
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        SerialPort1.Write("6")
+        Panel4.BackgroundImage = My.Resources.cuarto_apagado
+
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        SerialPort1.Write("9")
+        Panel10.BackgroundImage = My.Resources.cochera_prendido
+
+
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        SerialPort1.Write("8")
+        Panel10.BackgroundImage = My.Resources.cochera_apagado
+
+
+    End Sub
+End Class
